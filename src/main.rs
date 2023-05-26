722c78327f5e6037493ff847b9256150e33b9762
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-05-26 12:21:34 +0900

    firstcommit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod api;
+
+use api::task::get_task;
+
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+}
